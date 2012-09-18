94f382db13c750a60ab79b8a56dbfb083bd4a692
SwampWax <ajmar@udel.edu>
2012-09-18 15:34:22 -0400

    third commit

--- converter.cpp
+++ converter.cpp
@@ -7,12 +7,12 @@ intu convertToDecimal(char* conversionValue,intu sourceBase)
 	  values in the original string */
 	intu valueLength = strlen(conversionValue);
 	char* reverseValue = new char[valueLength];
-	for (int i = 0; i > valueLength; ++i)
+	for (int i = 1; i <= valueLength; ++i)
 	{
 		char swapChar = conversionValue[valueLength-i];
 		if (swapChar >= 97 /*a*/ || swapChar <= 122 /*z*/)
 		{
-			swapChar-=32;
+			swapChar-=32; // make it uppercase
 		}
 		else if (!(swapChar >= 48) && !(swapChar <= 57))
 		{
@@ -24,7 +24,7 @@ intu convertToDecimal(char* conversionValue,intu sourceBase)
 			fprintf(stderr,"You hit a sandbar and it destroyed your ship.");
 			exit(ERR_INVALIDCHAR);
 		}
-		reverseValue[i] = swapChar;
+		reverseValue[i-1] = swapChar;
 	}
 
 	/* Now that we've reversed our string and performed some
@@ -44,16 +44,16 @@ intu convertToDecimal(char* conversionValue,intu sourceBase)
 	while (1)
 	{
 		thisChar = *reverseValue++; counter++;
-		if (thisChar >= 97 /*a*/ || thisChar <= 122 /*z*/)
+		if (thisChar >= 65 /*A*/ && thisChar <= 90 /*Z*/)
 		{
 			thisChar -= 7;
 		}
-		else if (!(thisChar))
+		else if (thisChar == 0)
 		{
 			break;
 		}
 		intu char2Num = thisChar - 48;
-		if (thisChar >= sourceBase)
+		if (char2Num >= sourceBase)
 		{
 			fprintf(stderr,"Digit %u is too big for the specified base %u!\n",char2Num,sourceBase);
 			exit(ERR_OUTOFRANGE);
